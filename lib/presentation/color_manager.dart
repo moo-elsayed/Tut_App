eaac33fadbb2646286912d52ee8614f61baa7ccd
kk28xd <159706787+kk28xd@users.noreply.github.com>
2025-01-03 19:55:09 +0200

    add font & color manager

---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color primary = Color(0xffED9728); // Vibrant Orange
+  static const Color neutralGray = Color(0xff858585); // Neutral Gray
+  static const Color mediumGray = Color(0xff797979); // Medium Gray
+  static const Color lightSlateGray = Color(0xff9D9EA0); // Light Slate Gray
+  static const Color silver = Color(0xffC8C7CC); // Silver
+  static const Color darkGray = Color(0xff525252); // Dark Gray
+  static const Color ashGray = Color(0xff707070); // Ash Gray
+  static const Color charcoalGray = Color(0xff737477); // Charcoal Gray
+  static const Color tealBlue = Color(0xff304654); // Teal Blue
+}
